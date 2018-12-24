5586fbe02711597968efac5ea90ee62384e10c62
ZibonIbrahim <zibonibrahim@yahoo.com>
2018-12-24 13:27:01 -0500

    Math_PrimeNumber_submission

--- src/math/problems/PrimeNumber.java
+++ src/math/problems/PrimeNumber.java
@@ -10,6 +10,40 @@ package math.problems;
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 *
 		 */
+		ArrayList<Long> ar = new ArrayList<>();
+		long k = 0;
+		for (long i = 2; i < 1000000; i++) {
+		if (i == 2) {
+		System.out.println(i);
+		k++;
+		ar.add(i);
+
+		} else {
+		if (isPrime(i)) {
+		k++;
+		System.out.println(i);
+		ar.add(i);
+		}
+		}
+
+		}
+		System.out.println(" Total Number Of Prime Numbers: " + k);
+		System.out.println(ar);
+
+		List list = new ArrayList();
+		list = ar;
+
+        public static boolean isPrime(long n) {
+
+		if (n % 2 == 0) return false;
+		for (int i = 3; i * i <= n; i += 2) {
+
+		if (n % i == 0) return false;
+		}
+
+		return true;
+
+		}
 
 
 	}
