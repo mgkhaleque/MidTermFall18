e0517918162c648535458fd89f93af4ca84cf11f
ZibonIbrahim <zibonibrahim@yahoo.com>
2018-12-23 20:18:55 -0500

    Math-Factorial submission

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,24 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        //Factorial of 5
+        //using Iteration
+        int n = 5;
+        int result =1;
+        for (int i=1; i<=n;i++){
+            result = result * i;
+            System.out.println("The Factorial variable is: "+result);
+        }
+
+        //Factorial of 5
+        //using Recursion
+    }
+    public static int factorial(int n){
+        if (n==0){
+            return 1;
+        }else {
+            return n * factorial(n-1);
+        }
 
     }
 }
