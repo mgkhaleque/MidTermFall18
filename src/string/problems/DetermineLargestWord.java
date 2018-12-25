bdbf621fcfd1b60290ad53cd72eef0ab2c6f07c2
ZibonIbrahim <zibonibrahim@yahoo.com>
2018-12-24 21:13:25 -0500

    String_DetermineLargestWord

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -1,7 +1,6 @@
 package string.problems;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class DetermineLargestWord {
@@ -10,17 +9,40 @@ public class DetermineLargestWord {
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
-        String s="Human brain is a biological learning machine";
+        String s = "Human brain is a biological learning machine";
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
+        System.out.println("Word length from the given sentence is " + wordNLength);
+        System.out.println("Largest word from the given sentence is " + largestWord(s));
+
     }
 
-    public static Map<Integer, String> findTheLargestWord(String wordGiven){
+    public static Map<Integer, String> findTheLargestWord(String wordGiven) {
         Map<Integer, String> map = new HashMap<Integer, String>();
         String st = "";
         //implement
 
+        String[] ac = wordGiven.split(" ");
+
+        for (int i = 0; i < ac.length; i++) {
+            map.put(ac[i].length(), ac[i]);
+        }
+
         return map;
+    }
+
+    public static String largestWord(String givenWord) {
+        DetermineLargestWord dt = new DetermineLargestWord();
+        Map<Integer, String> wordNLength = dt.findTheLargestWord(givenWord);
+        int largest = 0;
+        String largeValue = "";
+        for (Map.Entry<Integer, String> checkLength : wordNLength.entrySet()) {
+            if (largest < checkLength.getKey()) {
+                largest = checkLength.getKey();
+                largeValue = checkLength.getValue();
+            }
+        }
+        return largeValue;
     }
 }
