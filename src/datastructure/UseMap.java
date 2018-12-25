d759a182344bb098e1b9405ccf6e60de266ae067
ZibonIbrahim <zibonibrahim@yahoo.com>
2018-12-25 13:17:53 -0500

    datastructure_UseMap subs

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,6 +12,45 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+
+		List<String> games = new ArrayList<>();
+		games.add("Bolla");
+		games.add("Chicky");
+		games.add("Micky");
+		games.add("War of game");
+
+		List<String> dc = new ArrayList<>();
+		dc.add("tigerman");
+		dc.add("ironman");
+		dc.add("Green Lantern");
+		dc.add("Flash");
+		
+		List<String> marvel = new ArrayList<>();
+		marvel.add("Leader");
+		marvel.add("Ironman");
+		marvel.add("Thor");
+		marvel.add("Hulk");
+
+		Map<String, List<String>> list = new HashMap<String, List<String>>();
+		list.put("Games",games);
+		list.put("DC", dc);
+		list.put("Marvel",marvel);
+
+		System.out.println(list);
+
+		System.out.println(" ");
+		System.out.println("Printout the values using for each loop.....");
+		for (Map.Entry<String, List<String>> st : list.entrySet()){
+			System.out.println(st.getKey()+ " --------> " +st.getValue());
+		}
+
+		System.out.println(" ");
+		System.out.println("Printout the values using iterator.....");
+		Set set = list.entrySet();
+		Iterator it = set.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
 
 	}
 
